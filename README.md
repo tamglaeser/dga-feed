9cd88eeeabfcbb6ed5dfd1600e8873ce1d5ef209
Tullia Glaeser <tullia.vinti@gmail.com>
2020-07-25 18:22:52 -0400

    added licenses, readme, and edited code

---
+++ README.md
@@ -0,0 +1,19 @@
+# Bambenek Feeds
+
+A python program for pulling down and parsing the information from the Bambenek Consulting High-Confidence C2 All Indicator Feed found 
+[here](https://osint.bambenekconsulting.com/feeds/) in the top table. This is the master feed of all current known, active and non-sinkholed C&C domains using DGAs.
+The data is structured in the following manner:
+- *domain*: the active non-sinkholed C&C domain that uses DGAS
+- *ip*: corresponding ip address
+- *nsname*: domain names of the autoritative nameserver (DNS) for the given DGA domain
+- *nsip*: corresponding ip addresses
+- *description*
+- *manpage*
+
+The program is made up of three functions which can be called by a menu: 1 to pull down all the information, 2 to pull down only the domain names, and 3 to pull down the domains and name server ips all in an array format; with this information, the domain names could be blacklisted and the malware and attacker potentially identified.
+
+## Getting Started
+
+The only prerequisite to running this program is having Python 3 which one can download [here](https://www.python.org/downloads/). Next, simply run 
+```python3 c2_all_indicator.py``` 
+to get the file running on your local server.
--- c2_all_indicator.py
+++ c2_all_indicator.py
@@ -1,64 +1,68 @@
 import urllib.request
 
 
+# function to pull out all the information on the C&C domains using DGAs
+# $domain, $ip, $nsname, $nsip, $description, $manpage
 def pull():
 
     url = "https://osint.bambenekconsulting.com/feeds/c2-masterlist-high.txt"  # data page
     file = urllib.request.urlopen(url)
 
-    complete = []
+    complete = []  # array containing all information on page
 
     for line in file:
         decoded_line = line.decode("utf-8")
 
-        if decoded_line[0] != '#':
+        if decoded_line[0] != '#':  # if past the beggining comments
             parts = decoded_line.split(',')
 
             domain = parts[0]
             ip = parts[1]
-            ns_host = parts[2].split('|')  # add array of new domains for each seed w/in new_domains array
-            ns_ip = parts[3].split('|')  # add array of new ip addresses for each seed w/in new_ips array
+            ns_host = parts[2].split('|')
+            ns_ip = parts[3].split('|')
             description = parts[4]
             manpage = parts[5]
 
-            each = [domain, ip, ns_host, ns_ip, description, manpage]  # array for each line
-            complete.append(each)  # complete array with whole feed's data
+            each = [domain, ip, ns_host, ns_ip, description, manpage]  # array for each line of info
+            complete.append(each)
 
     print(complete)
 
 
-# function to pull out only all the C&C domains using DGAs to block them
+# function to pull out only all the C&C domains using DGAs
+# -- could be used to to block them / put them on a blacklist
 def only_domain():
     url = "https://osint.bambenekconsulting.com/feeds/c2-masterlist-high.txt"  # data page
     file = urllib.request.urlopen(url)
 
-    dms = []
+    dms = []  # array to be filled w all the C&C domains on the page
 
     for line in file:
         decoded_line = line.decode("utf-8")
 
-        if decoded_line[0] != '#':
+        if decoded_line[0] != '#':  # past beginning comments
             parts = decoded_line.split(',')
             dms.append(parts[0])
 
     print(dms)
 
 
-# function to pull out C&C domains and name server ips to find malware and attacker
-# find malware : see who is asking any of those ns ips for that specific C&C domain -- could be malware
+# function to pull out C&C domains and name server ips
+# -- could be used to find malware and attacker
+# find malware : see who is querying any of those ns ips for that specific C&C domain -- could be malware
 # find attacker : find who registered that domain --
 # look at outed log to see who registered the C&C domain (which ip -- cld be ip of attacker)
 def domains_nsips():
     url = "https://osint.bambenekconsulting.com/feeds/c2-masterlist-high.txt"  # data page
     file = urllib.request.urlopen(url)
 
-    together = []
+    together = []  # array containing all pairs (C&C domain, nsips)
 
     for line in file:
         decoded_line = line.decode("utf-8")
 
         if decoded_line[0] != '#':
-            dm_nsips = []
+            dm_nsips = []  # [C&C domain, nsips] array
             parts = decoded_line.split(',')
             dm_nsips.append(parts[0])
             dm_nsips.append(parts[3].split('|'))
@@ -68,26 +72,31 @@ def domains_nsips():
 
 
 # main
-ans = True
+def main():
+    ans = True
 
-while ans:
-    print("""
-    1.Pull all information on active and non-sinkholed C&C domains using DGAs
-    2.Pull only domains of active and non-sinkholed C&C domains using DGAS -- to block
-    3.Pull domains of active and non-sinkholed C&C domains using DGAs and autoritative DNS names for the given DGA 
-    domain -- to find malware and attacker
-    4.Exit/Quit
-    """)
+    while ans:
+        print("""
+        1.Pull all information on active and non-sinkholed C&C domains using DGAs
+        2.Pull only domains of active and non-sinkholed C&C domains using DGAS -- to block
+        3.Pull domains of active and non-sinkholed C&C domains using DGAs and autoritative DNS names for the given DGA 
+        domain -- to find malware and attacker
+        4.Exit/Quit
+        """)
 
-    ans = input("What would you like to do? ")
-    if ans == "1":
-        pull()
-    elif ans == "2":
-        only_domain()
-    elif ans == "3":
-        domains_nsips()
-    elif ans == "4":
-        print("\n Goodbye")
-        ans = False
-    else:
-        print("\n Not Valid Choice, try again")
+        ans = input("What would you like to do? ")
+        if ans == "1":
+            pull()
+        elif ans == "2":
+            only_domain()
+        elif ans == "3":
+            domains_nsips()
+        elif ans == "4":
+            print("\n Goodbye")
+            ans = False
+        else:
+            print("\n Not Valid Choice, try again")
+
+
+if __name__ == '__main__':
+    main()
