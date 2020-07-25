0d70bb92f9cbf1bb460b6a7bb1d8368aa3aec820
Tullia Glaeser <tullia.vinti@gmail.com>
2020-07-25 13:09:16 -0400

    clean up format

--- c2_all_indicator.py
+++ c2_all_indicator.py
@@ -1,13 +1,12 @@
 import urllib.request
 
+
 def pull():
 
-    url = "https://osint.bambenekconsulting.com/feeds/c2-masterlist-high.txt" #data page
+    url = "https://osint.bambenekconsulting.com/feeds/c2-masterlist-high.txt"  # data page
     file = urllib.request.urlopen(url)
 
     complete = []
-    domain = ip = description = manpage = ""
-    ns_host = ns_ip = []
 
     for line in file:
         decoded_line = line.decode("utf-8")
@@ -15,28 +14,22 @@ def pull():
         if decoded_line[0] != '#':
             parts = decoded_line.split(',')
 
-
             domain = parts[0]
             ip = parts[1]
             ns_host = parts[2].split('|')  # add array of new domains for each seed w/in new_domains array
-            ns_ip = parts[3].split('|') # add array of new ip addresses for each seed w/in new_ips array
+            ns_ip = parts[3].split('|')  # add array of new ip addresses for each seed w/in new_ips array
             description = parts[4]
             manpage = parts[5]
 
-            each = [] # array for each line
-            each.append(domain)
-            each.append(ip)
-            each.append(ns_host)
-            each.append(ns_ip)
-            each.append(description)
-            each.append(manpage)
-            complete.append(each) # complete array with whole feed's data
+            each = [domain, ip, ns_host, ns_ip, description, manpage]  # array for each line
+            complete.append(each)  # complete array with whole feed's data
 
     print(complete)
 
-#function to pull out only all the C&C domains using DGAs to block them
+
+# function to pull out only all the C&C domains using DGAs to block them
 def only_domain():
-    url = "https://osint.bambenekconsulting.com/feeds/c2-masterlist-high.txt" #data page
+    url = "https://osint.bambenekconsulting.com/feeds/c2-masterlist-high.txt"  # data page
     file = urllib.request.urlopen(url)
 
     dms = []
@@ -50,11 +43,13 @@ def only_domain():
 
     print(dms)
 
+
 # function to pull out C&C domains and name server ips to find malware and attacker
-# find malware : see who is asking any of those nsips for that specific C&C domain -- could be malware
-# find attacker : find who registered that domain
+# find malware : see who is asking any of those ns ips for that specific C&C domain -- could be malware
+# find attacker : find who registered that domain --
+# look at outed log to see who registered the C&C domain (which ip -- cld be ip of attacker)
 def domains_nsips():
-    url = "https://osint.bambenekconsulting.com/feeds/c2-masterlist-high.txt" #data page
+    url = "https://osint.bambenekconsulting.com/feeds/c2-masterlist-high.txt"  # data page
     file = urllib.request.urlopen(url)
 
     together = []
@@ -72,18 +67,19 @@ def domains_nsips():
     print(together)
 
 
-
+# main
 ans = True
 
 while ans:
-    print ("""
+    print("""
     1.Pull all information on active and non-sinkholed C&C domains using DGAs
     2.Pull only domains of active and non-sinkholed C&C domains using DGAS -- to block
-    3.Pull domains of active and non-sinkholed C&C domains using DGAs and autoritative DNS names for the given DGA domain -- to find malware and attacker
+    3.Pull domains of active and non-sinkholed C&C domains using DGAs and autoritative DNS names for the given DGA 
+    domain -- to find malware and attacker
     4.Exit/Quit
     """)
 
-    ans=input("What would you like to do? ")
+    ans = input("What would you like to do? ")
     if ans == "1":
         pull()
     elif ans == "2":
